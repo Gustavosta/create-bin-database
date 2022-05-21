234822832e2e961b604244c1d1c8985a483c82e0
Gustavo Santana <43628256+Gustavosta@users.noreply.github.com>
2022-05-21 15:25:01 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,29 +4,20 @@ from bin_checker import bin_checker
 from concurrent.futures import ThreadPoolExecutor
 
 
-async def create_conn():
-    conn = await aiosqlite.connect('bins.db')
-    await conn.execute('CREATE TABLE IF NOT EXISTS bins (bin_number NUMERIC, bank TEXT, scheme TEXT, level TEXT, card_type TEXT, country TEXT)')
-    await conn.commit()
-    return conn
-
-
-conn = asyncio.run(create_conn())
-
-
 async def insert_bin(i):
     try:
-        query = await conn.execute('SELECT bin_number FROM bins WHERE bin_number = ?', (i,))
-        query = await query.fetchall()
-        if query is None or query == [] or query == ():
-            check = bin_checker(i)
-            if not check == ('', '', '', '', ''):
-                bank, scheme, level, card_type, country = check
-                await conn.execute('INSERT INTO bins VALUES (?, ?, ?, ?, ?, ?)', (i, bank, scheme, level, card_type, country))
-                await conn.commit()
-                print(f'{i} | Banco: {bank if not "" else "N/A"} - Bandeira:{scheme if not "" else "N/A"} - Nível: {level if not "" else "N/A"} - Tipo: {card_type if not "" else "N/A"} - País: {country if not "" else "N/A"}')
-            else:
-                print(f'{i} | Bin não existente')
+        async with aiosqlite.connect('bins.db') as conn:
+            query = await conn.execute('SELECT bin_number FROM bins WHERE bin_number = ?', (i,))
+            query = await query.fetchall()
+            if query is None or query == [] or query == ():
+                check = bin_checker(i)
+                if not check == ('', '', '', '', ''):
+                    bank, scheme, level, card_type, country = check
+                    await conn.execute('INSERT INTO bins VALUES (?, ?, ?, ?, ?, ?)', (i, bank, scheme, level, card_type, country))
+                    await conn.commit()
+                    print(f'{i} | Banco: {bank if not "" else "N/A"} - Bandeira:{scheme if not "" else "N/A"} - Nível: {level if not "" else "N/A"} - Tipo: {card_type if not "" else "N/A"} - País: {country if not "" else "N/A"}')
+                else:
+                    print(f'{i} | Bin não existente')
     except Exception as e:
         print('Erro ao inserir bin no banco de dados:', e)
 
@@ -45,15 +36,9 @@ async def create_database():
             pool.map(sync_insert_bin, bin_list)
     except Exception as e:
         print('Erro ao criar banco de dados:', e)
-        
-    await conn.close()
-
 
 if __name__ == '__main__':
     try:
         asyncio.run(create_database())
     except: pass
-    
-    asyncio.run(conn.close())
     print('Banco de dados criado com sucesso!')
-
